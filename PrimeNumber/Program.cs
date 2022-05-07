55935ae0a7cf1d5466a7ca4ebfa6e278f5b920b6
Emre Mert <emremert117@gmail.com>
2022-05-07 16:20:49 +0300

    added prime number project

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter a Number : ");
+int number = int.Parse(Console.ReadLine());
+bool IsPrime = true;
+int dividedBy = 2;
+
+for (int i = 2; i < number/2; i++)
+{
+    if (number % i == 0)
+    {
+        IsPrime = false;
+        dividedBy = i;
+        break;
+    }
+}
+
+if (IsPrime)
+    Console.WriteLine("Number is Prime.");
+else
+    Console.WriteLine($"Number is not Prime. It is divided by {dividedBy}");
+
+Console.ReadKey();
