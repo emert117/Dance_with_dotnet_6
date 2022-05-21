6846003cc9e4ca9437c6868db573b69a9af39b94
emert117 <emremert117@gmail.com>
2022-05-21 23:25:01 +0300

    rawstring

---
+++ RawStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿var sample = "{\"PropertyA\":\"Value\"}";
+var sample2 = """
+{"PropertyA":"Value"}
+""";
+
+var sample3 = """
+{
+    "PropertyA" : "Value"
+}
+""";
+
+
+System.Console.WriteLine(sample);
+System.Console.WriteLine(sample2);
+System.Console.WriteLine(sample3);
